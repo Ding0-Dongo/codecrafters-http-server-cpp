bb1bf6fba9a200a925f936634f27705b09313fab
DingoDongo <116123242+Ding0-Dongo@users.noreply.github.com>
2024-07-14 15:39:47 -0700

    Fixed size of content string

--- src/server.cpp
+++ src/server.cpp
@@ -61,7 +61,7 @@ int main(int argc, char **argv) {
 
   if(incomingMessage.starts_with("GET /echo/")){
     int endOfStr = incomingMessage.find("HTTP/1.1");
-    cout << endOfStr;
+    std::cout << endOfStr;
     contentStr = incomingMessage.substr(10, endOfStr - 1);
     std::string message = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(contentStr.size()) + "\r\n\r\n"+ contentStr;
     send(client_fd, message.c_str(), message.length(), 0);

52367a1dcfb59293f6c9e338e274be79b50fd8a1
DingoDongo <116123242+Ding0-Dongo@users.noreply.github.com>
2024-07-14 17:44:19 -0700

    Removing unnecessary couts

--- src/server.cpp
+++ src/server.cpp
@@ -62,9 +62,7 @@ int main(int argc, char **argv) {
   if(incomingMessage.starts_with("GET /user-agent HTTP/1.1\r\n")){
     int startOfStr = incomingMessage.find("User-Agent: ") + 12;
     int endOfStr = incomingMessage.find("\r\n", startOfStr);
-    std::cout << "End of str:" << std::to_string(endOfStr) << "\n";
     contentStr = incomingMessage.substr(startOfStr, endOfStr - startOfStr);
-    std::cout << "Content string: " << contentStr << "\n" << std::to_string(contentStr.size());
     std::string message = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(contentStr.size()) + "\r\n\r\n"+ contentStr;
     send(client_fd, message.c_str(), message.length(), 0);
   }

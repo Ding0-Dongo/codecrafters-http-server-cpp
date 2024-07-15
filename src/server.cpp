ce6bb3580bbbf1fd254bdc57afdf42d4a9baf3e6
DingoDongo <116123242+Ding0-Dongo@users.noreply.github.com>
2024-07-14 17:15:28 -0700

    Adding user agent return

--- src/server.cpp
+++ src/server.cpp
@@ -63,7 +63,7 @@ int main(int argc, char **argv) {
     int startOfStr = incomingMessage.find("User-Agent: ") + 12;
     std::cout << "Incoming message:" << incomingMessage << "\n\n";
     //std:: cout << std::to_string(startOfStr) << " " << std::to_string(endOfStr);
-    contentStr = incomingMessage.substr(startOfStr, endOfStr - 64);
+    //contentStr = incomingMessage.substr(startOfStr, endOfStr - 64);
     std::cout << contentStr << "\n" << std::to_string(contentStr.size());
     std::string message = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(contentStr.size()) + "\r\n\r\n"+ contentStr;
     send(client_fd, message.c_str(), message.length(), 0);
